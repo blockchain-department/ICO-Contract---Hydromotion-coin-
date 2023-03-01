a645a181ac5c97587406d34eabf2632232e526bd
Shuja Abrar <90973734+ShujaGraphy7@users.noreply.github.com>
2023-03-01 22:15:50 +0500

    push

---
+++ ERC20Token.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    uint256 maxSupply =  50000000000 * 10**2;
+    address presaleAddress;
+    constructor() ERC20("MyToken", "MTK") {}
+
+    function mint(address to) public onlyOwner {
+        // require(totalSupply() <= maxSupply, "Max Supply Reached");
+
+        _mint(to, maxSupply);
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        require(msg.sender == presaleAddress,"Only Called with Presale Smart Contract");
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
+        require(msg.sender == presaleAddress,"Only Called with Presale Smart Contract");
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function setPresaleAddress(address add) public onlyOwner{
+        presaleAddress = add;
+    }
+
+
+
+}
