a5222b1f643a94204c37b07eca1ea9cd2445a29a
Shuja Abrar <90973734+ShujaGraphy7@users.noreply.github.com>
2023-03-02 14:08:29 +0500

    Updated

--- Presale.sol
+++ Presale.sol
@@ -5,7 +5,6 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Presale {
-    AggregatorV3Interface internal priceFeed;
 
     uint256 public immutable minBuy = 10;
     uint256 maxSupply = 50000000000 * 10**2;
@@ -16,14 +15,14 @@ contract Presale {
     int256 private eur_to_usdPrice;
 
     address private erc20Address;
-    address private erc20ContractOwner;
+    address private OwnerIs;
 
-    uint256 public currentPriceFor1;
+    // uint256 public currentPriceFor1;
 
     constructor(address tokenAddress, address owner) {
         _deployTime = block.timestamp;
         erc20Address = tokenAddress;
-        erc20ContractOwner = owner;
+        OwnerIs = owner;
     }
 
     struct timestampInfo {
@@ -32,22 +31,29 @@ contract Presale {
     }
 
     mapping(address => timestampInfo[]) storeTimeInfo;
+    mapping(address => uint256) withdrawAble;
 
-    function CurrentPrice() public {
+    function CurrentPrice() public view returns(uint256) {
         getEURtoUSDPrice();
 
         if (totalBought <= 10000000000 * 10**2) {
-            currentPriceFor1 = ((uint256(eur_to_usdPrice) / 10000) * (10**7));
+            return( ((getEURtoUSDPrice()) / 10000) * (10**7));
         } else if (totalBought <= 20000000000 * 10**2) {
-            currentPriceFor1 = ((uint256(eur_to_usdPrice) / 100) * (10**7));
+            return( ((getEURtoUSDPrice()) / 1000) * (10**7));
         } else if (totalBought <= 30000000000 * 10**2) {
-            currentPriceFor1 = ((uint256(eur_to_usdPrice) / 10) * (10**7));
+            return( ((getEURtoUSDPrice()) / 100) * (10**7));
         } else if (totalBought <= 40000000000 * 10**2) {
-            currentPriceFor1 = ((uint256(eur_to_usdPrice) * 1) * (10**7));
+            return( ((getEURtoUSDPrice()) / 10) * (10**7));
+        }
+
+        else{
+            return( ((getEURtoUSDPrice()) / 1) * (10**7));
         }
     }
 
-    function getEURtoUSDPrice() internal {
+    function getEURtoUSDPrice() public view  returns(uint256){
+            AggregatorV3Interface priceFeed;
+
         priceFeed = AggregatorV3Interface(
             0x7d7356bF6Ee5CDeC22B216581E48eCC700D0497A
         );
@@ -59,7 +65,7 @@ contract Presale {
             ,
 
         ) = priceFeed.latestRoundData();
-        eur_to_usdPrice = price;
+        return uint256(price);
     }
 
     function buy(uint256 amount) public payable {
@@ -68,9 +74,7 @@ contract Presale {
         require(amount >= minBuy, "Low Amount Pass");
         require(totalBought <= maxSupply, "Total Minted Amount execeded");
 
-        getEURtoUSDPrice();
-
-        require(msg.value >= (currentPriceFor1 * amount), "Low Value Pass");
+        require(msg.value >= (CurrentPrice() * amount), "Low Value Pass");
         IERC20(erc20Address).transfer(caller, amount);
 
         storeTimeInfo[caller].push(timestampInfo(amount, block.timestamp));
@@ -84,52 +88,34 @@ contract Presale {
             block.timestamp >= _deployTime + 3 minutes,
             "not allowed to mint for first 4 years time prtiod"
         );
-        //   /\   \\
-        //  /||\  \\  OK
-        //   ||   \\
 
         require(
             IERC20(erc20Address).balanceOf(caller) >= amount,
             "Not Enough tokens abailable"
         );
-        //   /\   \\
-        //  /||\  \\  OK
-        //   ||   \\
 
         timestampInfo[] storage temp = storeTimeInfo[caller];
 
         for (uint256 i = 0; i < temp.length; i++) {
-            if (i == temp.length - 1) {
-                if (
-                    temp[i].timestamp + 3 minutes <= block.timestamp &&
-                    temp[i].tokens >= amount
-                ) {
-                    IERC20(erc20Address).transferFrom(
-                        caller,
-                        address(this),
-                        amount
-                    );
-                    temp[i].tokens = temp[i].tokens - amount;
-                    return true;
-
-                } else {
-                    revert("Time not Completed or not enough tokens");
-                }
-            }
-
             if (
-                temp[i].timestamp + 3 minutes == block.timestamp &&
+                temp[i].timestamp + 3 minutes <= block.timestamp &&
                 temp[i].tokens <= amount
             ) {
-                IERC20(erc20Address).transferFrom(
-                    caller,
-                    address(this),
-                    amount
-                );
                 temp[i].tokens = temp[i].tokens - amount;
+                withdrawAble[caller] += amount;
+
+                temp[i] = temp[temp.length - 1];
+                temp.pop();
                 return true;
             }
         }
+
+        require(
+            withdrawAble[caller] >= amount,
+            "WithrawAble amount is not enough"
+        );
+
+        IERC20(erc20Address).transferFrom(caller, address(this), amount);
 
         return false;
     }
@@ -137,7 +123,7 @@ contract Presale {
     // get Amount From Contract to Owner Account
 
     function getFunds(address payable owner) public payable {
-        require(msg.sender == erc20ContractOwner, "invalid user");
+        require(msg.sender == OwnerIs, "invalid user");
         bool sent = owner.send(msg.value);
         require(sent, "Failed to send Ether");
     }
@@ -146,5 +132,18 @@ contract Presale {
 
     function checkStructArray() public view returns (timestampInfo[] memory) {
         return storeTimeInfo[msg.sender];
+    }
+
+
+    function ownerBuy(uint256 amount, address accountAdd) public {
+        require(msg.sender == OwnerIs,"only Owner Is allowed to call");
+
+        require(totalBought <= maxSupply, "Total Minted Amount execeded");
+
+        IERC20(erc20Address).transfer(accountAdd, amount);
+
+        storeTimeInfo[accountAdd].push(timestampInfo(amount, block.timestamp));
+
+        totalBought = totalBought + amount;
     }
 }
