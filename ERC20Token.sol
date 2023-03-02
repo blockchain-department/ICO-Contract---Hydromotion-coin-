5f447d70b0348b4088575b56fa15e05ceb816833
Shuja Abrar <90973734+ShujaGraphy7@users.noreply.github.com>
2023-03-02 14:09:17 +0500

    Updated

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, ERC20Burnable, Ownable {
-    uint256 maxSupply =  50000000000 * 10**2;
+    uint256 maxSupply = 50000000000 * 10**2;
     address presaleAddress;
+
     constructor() ERC20("HydromotionCoin", "HYM") {}
 
     function mint(address to) public onlyOwner {
@@ -16,25 +17,37 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
         _mint(to, maxSupply);
     }
 
-    function transfer(address to, uint256 amount) public virtual override returns (bool) {
-        require(msg.sender == presaleAddress,"Only Called with Presale Smart Contract");
+    function transfer(address to, uint256 amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        require(
+            msg.sender == presaleAddress,
+            "Only Called with Presale Smart Contract"
+        );
         address owner = _msgSender();
         _transfer(owner, to, amount);
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
-        require(msg.sender == presaleAddress,"Only Called with Presale Smart Contract");
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        require(
+            msg.sender == presaleAddress,
+            "Only Called with Presale Smart Contract"
+        );
         address spender = _msgSender();
         _spendAllowance(from, spender, amount);
         _transfer(from, to, amount);
         return true;
     }
 
-    function setPresaleAddress(address add) public onlyOwner{
+    function setPresaleAddress(address add) public onlyOwner {
         presaleAddress = add;
     }
-
-
-
 }
